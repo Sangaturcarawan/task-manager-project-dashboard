ed7108fa6188ffa47472e3d23cf2a528a59b366f
Sangaturcarawan <marceau.ferrant@gmail.com>
2025-09-26 06:08:28 +0800

    Add 3 pages, template, fastapi, adding & deleting tasks

--- app/main.py
+++ app/main.py
@@ -1,9 +1,15 @@
-from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse
+from fastapi import FastAPI, Request, Form
+from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
+
+tasks = [
+    "Buy groceries",
+    "Finish project",
+    "Call the office"
+    ]
 
 app.mount("/static", StaticFiles(directory="app/static"), name="static")
 
@@ -15,12 +21,35 @@ async def read_root(request: Request):
     context = {
         "request": request,
         "username":"Faris",
-        "tasks": [
-            "Buy groceries",
-            "Finish project",
-            "Call mom"
-            ]
+        "tasks": tasks
     }
     return templates.TemplateResponse("index.html", context)
+
+@app.post("/add-task")
+async def add_task(task_name: str = Form(...)):
+    tasks.append(task_name)
+    return RedirectResponse(url="/", status_code=303)
+
+@app.post("/delete-task")
+async def delete_task(task_name: str = Form(...)):
+    if task_name in tasks:
+        tasks.remove(task_name)
+    return RedirectResponse(url="/", status_code=303)
+
+@app.get("/dashboard", response_class=HTMLResponse)
+async def read_dashboard(request: Request):
+
+    context = {
+        "request": request
+    }
+    return templates.TemplateResponse("dashboard.html", context)
+
+@app.get("/login", response_class=HTMLResponse)
+async def read_login(request: Request):
+
+    context = {
+        "request": request
+    }
+    return templates.TemplateResponse("login.html", context)
 
 
